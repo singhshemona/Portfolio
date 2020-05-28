72d35431041819da4a6cd5715d5a005660122c70
Shemona Singh <shemonas@indeed.com>
2020-05-28 09:36:01 -0400

    updated logo images, changed to svg

--- src/scripts/components/App/App.tsx
+++ src/scripts/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../ui/Header/Header';
 import Subtext from '../ui/Subtext/Subtext';
+import { ExternalLink } from '../ui/ExternalLink/ExternalLink';
 import './App.scss';
 
 function App() {
@@ -8,6 +9,10 @@ function App() {
     <div className="App">
       <Header />
       <Subtext />
+      <ExternalLink
+        link="mailto:singhshemona@gmail.com"
+        content="Let's Collaborate"
+      />
     </div>
   );
 }
--- src/scripts/components/ui/Header/Header.tsx
+++ src/scripts/components/ui/Header/Header.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import './Header.scss';
 import { ExternalLink } from '../ExternalLink/ExternalLink';
-import LinkedinLogo from 'images/linkedin.png';
+import LinkedinLogo from 'images/linkedin.svg';
+import NotionLogo from 'images/notion.svg';
+import DevToLogo from 'images/dev-logo.svg';
 
 const Header = () => {
   return (
     <header className="navigation">
-      {/* IS THERE A BETTER WAY TO USE IMAGES THEN THIS?? */}
-      <a className="social-media" href="https://www.linkedin.com/in/shemonasingh/" target="_blank" rel="noopener noreferrer">
-        <img src={LinkedinLogo} alt="Linkedin Logo" />
-      </a>
-
-      {/* MAKE LINKS LOOK LIKE ACTUAL LINKS, NOT BUTTONS. LEAVE BUTTON STYLING TO ACTUAL BUTTONS */}
-      <ExternalLink link="mailto:singhshemona@gmail.com" content="Let's Collaborate" />
-
-      <a className="button secondary-button" href="https://www.notion.so/shemona/Get-Started-cb739b6a13ce490ba1d422f204397071" target="_blank" rel="noopener noreferrer">My Second Brain</a>
-      {/* Put Dev.to logo */}
+      <ExternalLink
+        link="https://www.linkedin.com/in/shemonasingh/"
+        content={
+          <img src={LinkedinLogo} alt="Linkedin Logo" />
+        }
+      />
+      <ExternalLink
+        link="https://www.notion.so/shemona/Get-Started-cb739b6a13ce490ba1d422f204397071"
+        content={
+          <img src={NotionLogo} alt="Notion Logo" />
+        }
+      />
+      <ExternalLink
+        link="https://dev.to/singhshemona"
+        content={<img className="dev-to-logo" src={DevToLogo} alt="Dev to Logo" />}
+      />
     </header>
   );
 }
